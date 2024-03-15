401ef5e70ccc86cbd5e274313c50c8b5e1cf9d26
Chaitanya Pandey <chaitanya.pandey.python@gmail.com>
2024-03-15 14:33:31 +0530

    Remove unnecessary HTML code and fix file paths in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var dog=document.getElementById('dog_btn');
 dog.addEventListener('click',function(event){
     console.log("Woof!");
-    open('../public/dog.html','_self');
+    open('public/dog.html','_self');
 });
 var cat=document.getElementById('cat_btn');
 cat.addEventListener('click',function(event){
     console.log("Meow!");
-    open('../public/cat.html','_self');
+    open('public/cat.html','_self');
 });
