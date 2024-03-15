eb201c08bfbff639772ba5835f10e32ba0aed6ec
Chaitanya Pandey <chaitanya.pandey.python@gmail.com>
2024-03-15 14:18:58 +0530

    Update styles and add cat section

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ dog.addEventListener('click',function(event){
     console.log("Woof!");
     open('../public/dog.html','_self');
 });
+var cat=document.getElementById('cat_btn');
+cat.addEventListener('click',function(event){
+    console.log("Meow!");
+    open('../public/cat.html','_self');
+});
